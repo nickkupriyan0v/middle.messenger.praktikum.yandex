4f412393eda6a69fcbe2f54b8bd72c316a376d60
nickkupriyan0v <nickkuper@yandex.ru>
2025-07-25 18:57:51 +0300

    added header and refactored

---
+++ src/components/navigation.js
@@ -0,0 +1,12 @@
+export default `
+    <nav class=navigation>
+        {{> Link id="404-page" text="404"}}
+        {{> Link id="500-page" text="500"}}
+        {{> Link id="sign-up-page" text="Регистрация"}}
+        {{> Link id="sign-in-page" text="Вход"}}
+        {{> Link id="chats-page" text="Чаты"}}
+        {{> Link id="profile-page" text="Профиль"}}
+        {{> Link id="change-profile-page" text="Редактирование профиля"}}
+        {{> Link id="change-password-page" text="Редактирование пароля"}}
+    </nav>
+`;
