0850be420980ebd392f0aa15cf4a9e8a43377db1
nickkupriyan0v <nickkuper@yandex.ru>
2025-07-28 15:25:19 +0300

    improved avatar

--- src/ui-kit/avatar.js
+++ src/ui-kit/avatar.js
@@ -1,10 +1,19 @@
 import "../styles/avatar.scss";
 
 export default `
-    <div class="my-avatar {{#if editable}}editable{{/if}}">
-        <span class="letter">
-            {{letter}}
-        </span>
-        <i class="fas fa-gear"></i>
-    </div>
+    {{#if editable}}
+        <div id="avatar-uploader" class="my-avatar editable">
+            <span class="letter">
+                {{letter}}
+            </span>
+            <img class="preview-image" />
+            <input type="file" accept="image/*" hidden name="avatar" id="avatar-uploader-input" />
+        </div>
+    {{else}}
+        <div class="my-avatar">
+            <span class="letter">
+                {{letter}}
+            </span>
+        </div>
+    {{/if}}
 `;
